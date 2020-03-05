3bbc47d5d26d3f3f321b7a5232ce6bc5e60d0c86
Tanvir Walia <tarywalia@gmail.com>
2020-03-05 21:53:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,8 @@ def load_library(file_path)
   emoticons.each do |key,value|
   new_hash['get_emoticon'][value[0]] = emoticons[key][1]
   new_hash['get_meaning'][value[1]] = key
-
+end
+new_hash
   # code goes here
 end
 
